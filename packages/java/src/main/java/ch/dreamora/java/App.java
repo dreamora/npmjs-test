332400c2eebbb346674f0ca66ec0074f1ddecb7b
Marc Schärer <marc@dreamora.ch>
2022-07-19 14:11:37 +0200

    * Updates project to npm + java workspace

---
+++ packages/java/src/main/java/ch/dreamora/java/App.java
@@ -0,0 +1,13 @@
+package ch.dreamora.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
